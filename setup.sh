f6f4c19f5d4a6fc6a6b1165bcd3e2e63fa4ea281
Avinash_Pant <avinashpant57024@gmail.com>
2022-10-08 12:05:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,4 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-<<<<<<< HEAD
 " > ~/.streamlit/config.toml
-=======
-" > ~/.streamlit/config.toml
->>>>>>> 601b9950a241bc7721af0ebd6105c9a47eea32b1
